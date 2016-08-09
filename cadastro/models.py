1efb84b6fa8d601c03b171753e24b0dee9708195
Adeline Sharla <adelinesharla@gmail.com>
2016-08-09 17:08:39 -0300

    Criada classe tutor e seus campos no appcadastro

--- cadastro/models.py
+++ cadastro/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 'Esta classe implementa os campos de Tutor do Subsistema Secretaria e sua respectivas regras de negócio.'
 
+class Tutor(models.Model):
+	nome = models.CharField(max_length=50)
+	telefone = models.PositiveSmallIntergerField(max_length=11)
+	email = models.EmailField()
+	cpf = models.PositiveSmallIntegerField(max_length=11)
+

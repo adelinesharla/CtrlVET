5805244bc3003eec3106f1c08a3af0a5622bf131
Adeline Sharla <adelinesharla@gmail.com>
2016-08-09 16:39:27 -0300

    Criação do app cadastros

---
+++ cadastro/models.py
@@ -0,0 +1,6 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+'Esta classe implementa os campos de Tutor do Subsistema Secretaria e sua respectivas regras de negócio.'
+

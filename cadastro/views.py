7225507e3450965b848607c83268f9ba1066cbe6
silverbr23 <silverbr23@gmail.com>
2016-08-11 18:54:37 -0300

    views.py Atualizado
    Adicionado TutorFormView feito utilizando CBV conforme a documentação
    de djangoproject - https://docs.djangoproject.com/en/1.9/topics/class-based-views/intro/

--- cadastro/views.py
+++ cadastro/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.conf.urls import url
+'HttpResponse para uma pagina template indicando que a operação foi realizada (verificar se tal página existe)'
+from django.http import HttpResponseRedirect
+'Imortando Formularios necessários para views de tutor e animal'
+from .forms import TutorModelForm, AnimalModelForm
 
+
+
+class TutorFormView(View):
+  form_class_tutor = TutorModelForm
+  template_formulario = 'form.html'
+  
+  def get(self, request):
+    form = self.form_class()
+    return render(request, self.template_formulario, {'form':form})
+  
+  def post(self, request):
+    form = self.form_class(request.POST)
+    if form.is_valid():
+      return HttpResponseRedirect('/success/')
+    
+    return render(request, self.template_formulario, {'form':form})
+    
+    
 # Create your views here.

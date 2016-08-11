5ed53436e46bb888d9394df7bcb57e893e81060a
silverbr23 <silverbr23@gmail.com>
2016-08-11 18:59:07 -0300

    views.py Atualizado
    Adicionado a view de AnimalFormView

--- cadastro/views.py
+++ cadastro/views.py
@@ -6,9 +6,24 @@ from django.http import HttpResponseRedirect
 from .forms import TutorModelForm, AnimalModelForm
 
 
-
+'TUTOR FORM VIEW , PARA FORMULARIO DE CADASTRO DO TUTOR'
 class TutorFormView(View):
   form_class_tutor = TutorModelForm
+  template_formulario = 'form.html'
+  
+  def get(self, request):
+    form = self.form_class()
+    return render(request, self.template_formulario, {'form':form})
+  
+  def post(self, request):
+    form = self.form_class(request.POST)
+    if form.is_valid():
+      return HttpResponseRedirect('/success/')
+    
+    return render(request, self.template_formulario, {'form':form})
+'ANIMAL FORM VIEW, PARA FORMULARIO DE CADASTRO DO ANIMAL'    
+class AnimalFormView(View):
+  form_class_animal = AnimalModelForm
   template_formulario = 'form.html'
   
   def get(self, request):

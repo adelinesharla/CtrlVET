39393612f495f19262b736502e8fd9ea4f0a51ba
joyce <joyce.ambrosio@gmail.com>
2016-08-12 17:21:22 -0300

    Template de form de cadastro de animal e tutor corrigidos.

--- cadastro/views.py
+++ cadastro/views.py
@@ -6,6 +6,11 @@ from django.views.generic import View, TemplateView, DetailView, UpdateView
 from django.http import HttpResponseRedirect
 'Imortando Formularios necessários para views de tutor e animal'
 from .forms import TutorModelForm, AnimalModelForm
+from .models import *
+
+#Não somos selvagens. O uso de 2 espaços como forma de identação é degradante.
+#Tabs são apropriados e garantem uma maior readability ao código
+# :)
 
 """Classe de renderização da main (sem contexto)"""
 class MainView(TemplateView):
@@ -13,28 +18,28 @@ class MainView(TemplateView):
 
 'TUTOR FORM VIEW , PARA FORMULARIO DE CADASTRO DO TUTOR'
 class TutorFormView(View):
-  form_class_tutor = TutorModelForm
-  template_formulario = 'cadastro/form.html'
-  
-  def get(self, request):
-    form = self.form_class_tutor()
-    return render(request, self.template_formulario, {'form':form})
-  
-  def post(self, request):
-    form = self.form_class(request.POST)
-    if form.is_valid():
-      return HttpResponseRedirect('/success/')
-    
-    return render(request, self.template_formulario, {'form':form})
+	form_class_tutor = TutorModelForm
+	template_formulario = 'cadastro/form.html'
+
+	def get(self, request):
+		form = self.form_class_tutor()
+		return render(request, self.template_formulario, {'form':form})
+
+	def post(self, request):
+		form = self.form_class(request.POST)
+		if form.is_valid():
+			return HttpResponseRedirect('/success/')
+		return render(request, self.template_formulario, {'form':form})
+
 
 """Classe para retornar detalhes de Tutor (alimenta o template tutor_detalhes)"""
 class TutorDetalhesView(DetailView):
-
 	model = Tutor
-	
+
 	def get_context_data(self):
 		context = super (TutorDetalhesView, self).get_context_data()
 		return context
+
 """Classe para editar Tutor"""
 class TutorEditar(UpdateView):
 	model = Tutor
@@ -42,19 +47,20 @@ class TutorEditar(UpdateView):
 
 'ANIMAL FORM VIEW, PARA FORMULARIO DE CADASTRO DO ANIMAL'    
 class AnimalFormView(View):
-  form_class_animal = AnimalModelForm
-  template_formulario = 'cadastro/form.html'
-  
-  def get(self, request):
-    form = self.form_class_animal()
-    return render(request, self.template_formulario, {'form':form})
-  
-  def post(self, request):
-    form = self.form_class(request.POST)
-    if form.is_valid():
-      return HttpResponseRedirect('/success/')
-    
-    return render(request, self.template_formulario, {'form':form})
+	form_class_animal = AnimalModelForm
+	template_formulario = 'cadastro/form.html'
+
+	def get(self, request):
+		form = self.form_class_animal()
+		return render(request, self.template_formulario, {'form':form})
+
+	def post(self, request):
+		form = self.form_class(request.POST)
+		if form.is_valid():
+			return HttpResponseRedirect('/success/')
+	
+		return render(request, self.template_formulario, {'form':form})
+
 """Classe para retornar detalhes de Animal (alimenta o template animal_detalhes)"""
 class AnimalDetalhesView(DetailView):
 

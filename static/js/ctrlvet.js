86f9da35c8f28f3013160bacdae728a97d935b01
joyce <joyce.ambrosio@gmail.com>
2016-09-14 16:16:16 -0300

    Adicionado js de modal.

--- static/js/ctrlvet.js
+++ static/js/ctrlvet.js
@@ -7,4 +7,11 @@ $( document ).ready(function() {
 	$(".dropdown-button").dropdown();
 	$('select').material_select();
 	$('input:not([placeholder])').attr('placeholder', '');
+
+	$('.modal-trigger').leanModal({
+		dismissible: true, 
+		opacity: .5, 
+		in_duration: 300,
+		out_duration: 200, 
+	});
 });

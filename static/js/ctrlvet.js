dc41cf2a92d3159e4c7340e97e2fd692319bb4b1
joyceambrosio <joyce.ambrosio@gmail.com>
2016-10-15 23:36:36 -0300

    Datetimepicker em pt-br, regra de negocio para datas implementada.
    Data de aniversário não pode ser maior que a marcação.
    Data de agendamento de consulta não pode ser menor que a data atual.

--- static/js/ctrlvet.js
+++ static/js/ctrlvet.js
@@ -14,20 +14,29 @@ $( document ).ready(function() {
 		in_duration: 300,
 		out_duration: 200, 
 	});
+	
+	jQuery.fn.datetimepicker.defaults['lang'] = 'pt-Br';
+	jQuery.fn.datetimepicker.defaults['maxDate'] = 0;
+
+
+
 });
 
 function AlertSucesso(){
-	  flag = 1;
-	  for (i = 0; i < document.forms[0].getElementsByTagName('input').length; i++ ){
-		  if( document.forms[0][i].value == null || document.forms[0][i].value == "" ){
-				flag = 0;
-		  }
-	  }
-    if (flag == 0) {
-        alert("Erro no formulário!");
-        return false;
-    }else{
+	flag = 1;
+	for (i = 0; i < document.forms[0].getElementsByTagName('input').length; i++ ){
+		if( document.forms[0][i].value == null || document.forms[0][i].value == "" ){
+			flag = 0;
+		}
+	}
+	if (flag == 0) {
+		alert("Erro no formulário!");
+		return false;
+	}else{
 		alert("Sucesso! clique 'ok' para voltar ao resumo!");
 		return true;
 	}
-	};
+};
+
+
+

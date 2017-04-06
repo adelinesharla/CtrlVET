0311735e18aa260a80d1d34713e990c1a8295afb
Adeline Sharla <adelinesharla@gmail.com>
2017-04-06 16:50:09 -0300

    criada associação de registro de login para veterinários e tecnicos

--- login/views.py
+++ login/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
-
+from cadastro.models import Veterinario, Tecnico
 from forms import SignUpForm
 
 def signup(request):
@@ -12,6 +12,12 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
+            if form.cleaned_data.get('tipo') == 'Tec':
+            	tec = Tecnico(nome=form.cleaned_data.get('username'), email=form.cleaned_data.get('email'), cpf=form.cleaned_data.get('cpf'), crf=form.cleaned_data.get('registro'))
+            	tec.save()
+            else:
+            	vet = Veterinario(nome=form.cleaned_data.get('username'), email=form.cleaned_data.get('email'), cpf=form.cleaned_data.get('cpf'), crmv=form.cleaned_data.get('registro'))
+            	vet.save()
             return redirect('main')
     else:
         form = SignUpForm()

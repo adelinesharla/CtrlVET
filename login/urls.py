1189a931732fd1738722a07b0da0f370c363cb3c
joyceambrosio <joyce.ambrosio@gmail.com>
2017-04-29 14:31:53 -0300

    Telas main e de login e cadastro criadas

--- login/urls.py
+++ login/urls.py
@@ -1,10 +1,14 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from login.views import *
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^main/signup/$', signup, name='signup'),
     url(r'^main/login/$', auth_views.login,{'template_name': 'login/login.html'}, name='login'),
     url(r'^main/logout/$', auth_views.logout, {'template_name': 'login/logout.html'},name='logout'),
+    url(r'^main/veterinario/$', TemplateView.as_view(template_name='veterinario_auth.html')),
+    url(r'^main/secretario/$', TemplateView.as_view(template_name='secretario_auth.html')),
+    url(r'^main/tecnico/$', TemplateView.as_view(template_name='tecnico_auth.html')),
 ]
